6752778b1707c050e42e1c66036d254d5840a8b8
vikashkumar6207 <vktd828403@gmail.com>
2023-11-24 16:49:11 +0530

    file add

---
+++ factorial.java
@@ -0,0 +1,14 @@
+class factorial{
+    public static void main(String[] args) {
+        int n = 36;
+        int sqrt = (int)Math.sqrt(n);
+        for(int i = 1;i <= sqrt;i++){
+            if(n%i == 0){
+                System.out.println(i);
+                if(n/i != i){ 
+                System.out.println(n/i+"");
+                }
+            }
+        }
+    }
+}
