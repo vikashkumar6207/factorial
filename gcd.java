45ab3b3cc09e8c1923a9054ec5c949ef463e200e
vikashkumar6207 <vktd828403@gmail.com>
2023-11-25 12:33:02 +0530

    file add

--- gcd.java
+++ gcd.java
@@ -1,21 +1,27 @@
 public class gcd {
 
-    // GCD
-    public static int greatestCD(int a, int b){
-        while(b != 0){
-            int fact = a%b;
-            a = b;
-            fact = b;
-        }
-        return a;
+    // // GCD
+    public static int greatestCD(int a, int b) {
+    while (b != 0) {
+    int fact = a % b;
+    a = b;
+    b = fact;
+    }
+    return a;
     }
 
-    // LCM
-    public static int Lcm(int a, int b){
-        return (a * b) / greatestCD(a, b);
+    // // LCM
+    public static int Lcm(int a, int b) {
+    return (a * b) / greatestCD(a, b);
     }
+
     public static void main(String[] args) {
-        // int arr = greatestCD(21, 24);
+        int arr = greatestCD(273, 315);
+        System.out.println(arr);
+        int lcm = Lcm(2652, 3060);
         System.out.println(Lcm(18, 24));
+        System.out.println(lcm);
+        
     }
+
 }
